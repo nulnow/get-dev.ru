2ea5896eb3775c0757cc6314ca09fdce844b10ba
Andrey Razuvaev <andysikt@gmail.com>
2019-04-06 09:15:04 +0300

    Ссылка на профил в таске

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -6,10 +6,12 @@
 
     <div class="mt-10 container block shadow-1 text">
         <div class="task">
-            <div class="task__owner">
+            <a href="{{ route('user', $task->owner->id) }}" class="task__owner">
                 <img src="{{ $task->owner->img }}" class="task__owner-photo" alt="">
-                <h4 class="task__owner-name">{{ $task->owner->name }}</h4>
-            </div>
+                <h4 class="task__owner-name">
+                    {{ $task->owner->name }}
+                </h4>
+            </a>
             <h1>{{ $task->title }}</h1>
             <p>{{ $task->description }}</p>
         </div>
