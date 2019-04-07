e0f76e06b92899e1e9fbba2aaa5f12e1c895b170
Andrey Razuvaev <andysikt@gmail.com>
2019-04-07 03:06:32 +0300

    welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,21 +3,37 @@
 @section('content')
 
     @guest
-        <div class="mt-10 container-fluid shadow-1 jumbotron jumbotron--welcome" style="background-image: url('http://www.wallpaperback.net/wp-content/uploads/2018/08/Wallpaper%20Samsung%20Galaxy%20Note%209,%20Android%208.0,%20Android%20Oreo,%20abstract,%20colorful,%20Abstract%209280718172.jpg');">
-            <div class="container">
-                <h1>Краудфандинг</h1>
-                <p>Nihil verum est licet omnia.</p>
+        <div style="
+            background-image: url('https://media.giphy.com/media/G06qqg0VZnvLG/giphy.gif');
+            -webkit-background-size: cover;
+            background-size: cover;
+            height: 550px;
+        ">
+            <div class="container" style="color: white;">
+                <h1
+                    style="
+                        margin-top: 20vh;
+                        background-color: #212121;
+                        font-size: 40px;
+                        padding: 10px;
+                    "
+                >Краудфандинг нового уровня</h1>
+                <p
+                    style="
+                        background-color: rgba(0,0,0,0.8);
+                    "
+                >Нескучные обои, чат, копатыч гарантирует</p>
             </div>
         </div>
-        <div class="container block shadow-1">
-            <div class="advantage">
-                <img src="https://images.vexels.com/media/users/3/132826/isolated/preview/5b6683288fdfe1a87f3a7dbbd302fba9-zoom-tool-by-vexels.png" alt="">
-                <p>Поиск разработчиков Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eos harum iure libero minus necessitatibus nobis nostrum, numquam obcaecati ut.</p>
-            </div>
-            <div class="advantage">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab at beatae deserunt nam pariatur sunt? Exercitationem nobis odit quo velit?</p>
-                <img src="https://image.flaticon.com/icons/png/512/37/37631.png" alt="">
-            </div>
+        <div class="container">
+            {{--<div class="advantage">--}}
+                {{--<img src="https://images.vexels.com/media/users/3/132826/isolated/preview/5b6683288fdfe1a87f3a7dbbd302fba9-zoom-tool-by-vexels.png" alt="">--}}
+                {{--<p>Поиск разработчиков Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eos harum iure libero minus necessitatibus nobis nostrum, numquam obcaecati ut.</p>--}}
+            {{--</div>--}}
+            {{--<div class="advantage">--}}
+                {{--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab at beatae deserunt nam pariatur sunt? Exercitationem nobis odit quo velit?</p>--}}
+                {{--<img src="https://image.flaticon.com/icons/png/512/37/37631.png" alt="">--}}
+            {{--</div>--}}
         </div>
     @else
 
